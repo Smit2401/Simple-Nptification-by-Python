b655157a50b17031758d0e90cc927762892f9b87
Smit2401 <112608590+Smit2401@users.noreply.github.com>
2023-08-28 23:21:52 +0530

    drink_water_reminder.py
    just a normal update for better understanding for user

--- drink_water_reminder.py
+++ drink_water_reminder.py
@@ -2,7 +2,7 @@ from winotify import Notification,audio
 
 a = Notification(app_id= "Drink Water",
                  title= "It's been 2 Hours",
-                 msg="Hey Smit! Drink Some Water!",
+                 msg="Hey (Your_name)! Drink Some Water!",
                  duration="long",
                  icon=r"C:\Users\smit\Desktop\Pyhton\6774894.png")
 
